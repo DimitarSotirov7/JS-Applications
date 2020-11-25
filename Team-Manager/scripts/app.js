ed68a77865b8921719e323aecec673b2e026be4c
DimitarSotirov7 <dimitarsotirov7@gmail.com>
2020-11-25 16:05:22 +0200

    Team-Manager: Add js file
    HTML elements controller

--- Team-Manager/scripts/app.js
+++ Team-Manager/scripts/app.js
@@ -1,1 +1,1 @@
-
+import elements from './htmlElements.js';
---
+++ Team-Manager/scripts/htmlElements.js
@@ -0,0 +1,6 @@
+const elements = {
+    infoBox: document.querySelector('#infoBox'),
+    errorBox: document.querySelector('#errorBox')
+};
+
+export default elements;
