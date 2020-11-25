d5d5e4c61a65ebdb1611c3adf5890b7a8eabeeef
DimitarSotirov7 <dimitarsotirov7@gmail.com>
2020-11-25 16:16:01 +0200

    Team-Manager: Add post requests
    Register
    Login

--- Team-Manager/scripts/app.js
+++ Team-Manager/scripts/app.js
@@ -53,6 +53,45 @@ function sammyFunc() {
             });
     });
 
+    // ---------- POST ----------
+
+    this.post('/register', function (context) {
+        const { email, password, repeatPassword } = context.params;
+
+        if (password !== repeatPassword) {
+            const message = 'The password must be equal to repeatPassword!';
+            errorHandler(message);
+            return;
+        }
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((user) => {
+                this.redirect('/login');
+            })
+            .catch(err => errorHandler(err.message));
+    });
+
+    this.post('/login', function (context) {
+
+        const { email, password } = context.params;
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(({ user: { uid, email } }) => {
+
+                localStorage.setItem('userInfo', JSON.stringify({ email, password }));
+                this.redirect('/home');
+            })
+            .catch(err => errorHandler(err.message));
+    });
 }
 
 router.run('/home');
+
+function errorHandler(message) {
+    elements.errorBox.textContent = message;
+    elements.errorBox.style.display = 'block';
+
+    setTimeout(() => {
+        elements.errorBox.style.display = 'none';
+    }, 3000)
+}
